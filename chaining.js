c4f3c5d98952682d432587c46cd3b52835abee95
Mitchell Blom <mitchellblom@gmail.com>
2017-02-13 20:10:43 -0600

    class notes current with examples

--- chaining.js
+++ chaining.js
@@ -10,12 +10,27 @@ console.log("Integers descending in value: ", integersDescending);
 
 // 2.  Remove any integers greater than 19.
 
-function lessThan19 (integers) {
-	var filteredResults = [];
-	for (var i = 0; )
-}
+var lessThan19 = integers.sort(function(a,b) {return b-a}).filter(function(num){return num < 19});
+
+console.log("Integers less than 19: ", lessThan19)
 
 
 // 3.  Multiply each remaining number by 1.5 and then subtract 1.
+
+var timesMinus = integers.sort(function(a,b) {return b-a}).filter(function(num){return num < 19}).map(function(num){return (num * 1.5)-1});
+
+	console.log("Times 1.5 then Subtract 1: ", timesMinus);
+
 // 4.  Then output (either in the DOM or the console) the sum of all the resulting numbers.
 
+var sumResulting = integers.sort(function(a,b) {return b-a}).filter(function(num){return num < 19}).map(function(num){return (num * 1.5)-1}).reduce(function(prev, curr){return prev + curr;});
+
+console.log("Sum of the resulting numbers: ", sumResulting);
+
+
+// Making 4 neater, but doing the same thing without console logging:
+
+var chainingResult = integers.sort(function(a,b) {return b-a})
+	.filter(function(num){return num < 19})
+	.map(function(num){return (num * 1.5)-1})
+	.reduce(function(prev, curr){return prev + curr;});
